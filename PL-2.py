d9b37afdd9fbbd261f9eef876c9c4d62c840ca3c
TDLaw007 <lawtra820@gmail.com>
2025-05-07 03:17:17 +0600

    Update PL-2.py

--- PL-2.py
+++ PL-2.py
@@ -1,24 +1,15 @@
 # Data Types
 
-
 # 1. strings
 # print(type("Hello world"))
 # print("Hello World"[7])
 
-
 # 2. integer
 # print(type(7))
-
 
 # 3. Float
 # print(type(5.67))
 
-
 # Boolean
 # True or False
-
-
 # Conditional Statement
-# if statement.
-# elif statement.
-# else statement.
